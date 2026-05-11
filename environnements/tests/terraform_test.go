936f0ebe8ea6a3a551e89ab248052421f3016c3e
samsteeven <samendjiaha@gmail.com>
2026-05-11 16:43:48 +0100

    Ajout du pipeline CI/CD GitHub Actions

---
+++ environnements/tests/terraform_test.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	"testing"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerraformAgriCam(t *testing.T) {
+	t.Parallel()
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../environnements/dev",
+		Vars: map[string]interface{}{
+			"environnement": "test",
+			"type_instance": "t2.micro",
+		},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Vérifie que le VPC a été créé
+	vpcId := terraform.Output(t, terraformOptions, "id_vpc")
+	assert.NotEmpty(t, vpcId)
+}
