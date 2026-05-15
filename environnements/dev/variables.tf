69fcb997de01d10310758ebae10281a8e5681cdf
samsteeven <samendjiaha@gmail.com>
2026-05-15 15:51:12 +0100

    enlever la variable inutile ami_id puisqu'on utilise dejà la recherche automatique pour trouver l'image ubuntu

--- environnements/dev/variables.tf
+++ environnements/dev/variables.tf
@@ -17,11 +17,7 @@ variable "type_instance" {
   default     = "t2.micro"  # Gratuite dans le Free Tier AWS
 }
 
-variable "ami_id" {
-  description = "ID de l'AMI (Amazon Machine Image) pour l'instance EC2"
-  type        = string
-  default     = "ami-0c1c4a6a"  # Ubuntu 22.04 LTS en af-south-1 (à vérifier)
-}
+
 
 variable "ip_admin" {
   description = "Adresse IP de l'administrateur autorisé à se connecter en SSH (format CIDR : X.X.X.X/32)"
